68b7ca28f5c08047ff8a7447f4975d16e1575504
halki <s.halki0203@icloud.com>
2020-06-16 22:40:03 +0900

    up

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
+    <LikeHeader></LikeHeader>
     <LikeNumber></LikeNumber>
     <test></test>
   </div>
 </template>
 
 <script>
+  import LikeHeader from './components/LikeHeader';
 
+  export default {
+    components: {
+      LikeHeader
+    }
+  }
 </script>
 
 <style>
---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>{{ good }}</p>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      good: 'いいね！！！'
+    };
+  }
+};
+</script>
