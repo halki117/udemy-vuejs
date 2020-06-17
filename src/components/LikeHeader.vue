485bdd5afc2bb27f6e7581b5a7606afaaf6ca6d9
halki <s.halki0203@icloud.com>
2020-06-17 21:46:52 +0900

    up

--- src/components/LikeHeader.vue
+++ src/components/LikeHeader.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <slot></slot>
+    <p>{{ headerText }}</p>
   </div>
 </template>
 
 // <script>
-// export default {
-//   data: function() {
-//     return {
-//       good: 'いいね！！！'
-//     };
-//   }
-// };
-// </script>
+export default {
+  props: ["headerText"]
+  // data: function() {
+  //   return {
+  //     good: 'いいね！！！'
+  //   };
+  // }
+};
+</script>
